70f3d1dc3b3bef8e627533cad01920351536b3b9
Swati Tanu <swatitani01@gmail.com>
2023-11-19 21:53:35 +0530

    Filter Feature

--- 5.Let'sGetHooked/Notes.md
+++ 5.Let'sGetHooked/Notes.md
@@ -1,4 +1,4 @@
-####Two types of Export/Import
+### Two types of Export/Import
 
 1. Default Export/Import
 export default variable_name;
@@ -12,4 +12,13 @@ When exporting two or more thing then only use Named Export/Import.
 
 Fun question: Can we use a Default Export/Import along with Named Export/Import?
 Answer: Yes
+
+### React Hooks
+It is a normal JS utility function given to us React (pre-built) and that functions has some logics already written to it. It is utility function given to us by React
+
+#### 2 most important hooks are:
+1. useState() - It gives superpowerful State Variable in React. Whenever a State Variable updates React re-renders the component.
+2. useEffect()
+
+
 
--- 5.Let'sGetHooked/src/components/Body.js
+++ 5.Let'sGetHooked/src/components/Body.js
@@ -1,21 +1,27 @@
 import RestaurantCard from "./Restraurant";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 //Body Component
 const Body = () => {
+
+  //State Variable - Super powerful variable using Hooks
+  const [listOfRestraunt, setListOfRestraunt] = useState(resList)
+
   return (
     <div className="body">
       <div className="fitler">
         <button className="filter-btn" onClick={()=>{
-          console.log("Button Clicked!")
-          resList = resList.filter((res)=>res.data.avgRating > 4);
-          console.log(resList);
+          // console.log("Button Clicked!")
+          const filteredList = listOfRestraunt.filter((res)=>res.data.avgRating > 4);
+          setListOfRestraunt(filteredList)
+          // console.log(filteredList);
         }}>
           Top Rated Restraurant
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestraunt.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
