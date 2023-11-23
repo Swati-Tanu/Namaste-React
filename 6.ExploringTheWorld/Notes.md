bfbd9b49b629a7b5e24af02fa86db49221135490
Swati Tanu <swatitani01@gmail.com>
2023-11-23 09:25:23 +0530

    useEffect

--- 6.ExploringTheWorld/Notes.md
+++ 6.ExploringTheWorld/Notes.md
@@ -2,5 +2,10 @@
 
 Microservice Architecture is also known as seperation of concern, and follows single responsibility principle.
 
+In React for fetching data from API snd displaying we use the following approach:
+
+Loads -> Render -> API -> Re-render
+
+#### useEffect is a React Hook that calls a callback function once the component is already rendered.
 
 
--- 6.ExploringTheWorld/src/components/Body.js
+++ 6.ExploringTheWorld/src/components/Body.js
@@ -1,23 +1,41 @@
 import RestaurantCard from "./Restraurant";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 //Body Component
 const Body = () => {
-
-  //State Variable - Super powerful variable using Hooks
-  const [listOfRestraunt, setListOfRestraunt] = useState(resList)
+  const [listOfRestraunt, setListOfRestraunt] = useState(resList || [])
   
-  //The above line is same as the below:
-  // const arr = useState(resList)
-  // const [listOfRestraunt, setListOfRestraunt] = arr;
+  //The two parameter in useEffect are first one is a callback function and a dependency array.
+
+  useEffect(() => {
+    /*This callback function is called after the component renders*/
+    // console.log("useEffect Called")
+    // fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      // console.log(json)
+      console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+      setListOfRestraunt(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+if(listOfRestraunt.length === 0) {
+  return Shimmer
+  // <h1>Loading...</h1>
+}
 
   return (
     <div className="body">
       <div className="fitler">
         <button className="filter-btn" onClick={()=>{
           // console.log("Button Clicked!")
-          const filteredList = listOfRestraunt.filter((res)=>res.data.avgRating > 4);
+          const filteredList = listOfRestraunt.filter((res)=> res.data.avgRating > 4);
           setListOfRestraunt(filteredList)
           // console.log(filteredList);
         }}>
