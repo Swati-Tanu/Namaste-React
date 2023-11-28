840b5a2b80b24963f30975e0e052f9c6d0e223cf
Swati Tanu <swatitani01@gmail.com>
2023-11-28 18:18:01 +0530

    Restaurant Menu

--- 7.FindingThePath/src/components/RestaurantMenu.js
+++ 7.FindingThePath/src/components/RestaurantMenu.js
@@ -1,22 +1,36 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const RestuarantMenu = () => {
+
+    const [resInfo, setResInfo] = useState(null);
 
     useEffect(()=>{
         fetchMenu();
     }, [])
 
     const fetchMenu = async () => {
-        const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch('https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&restaurantId="236434"&submitAction=ENTER');
         
         const json = await data.json();
 
-        console.log(json)
+        // console.log(json)
+
+        setResInfo(json.data);
     }
 
-    return (
+    // if(resInfo === null) return <Shimmer/>;
+
+    // const {name, cuisines, costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
+
+    return (resInfo === null) ? (
+        <Shimmer/> 
+        ) : (
         <div className="menu">
             <h1>Name of the Resturant</h1>
+            {/* <h1>{name}</h1> */}
+            {/* <h3>{cuisines.join(",")}</h3> */}
+            {/* <h3>{costForTwoMessage}</h3> */}
             <h2>Menu</h2>
             <ul>
                 <li>Biryani</li>
