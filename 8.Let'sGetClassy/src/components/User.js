070b9e396c224f894b55d65826964b2617040ca3
Swati Tanu <swatitani01@gmail.com>
2023-12-02 13:42:02 +0530

    State Variable using class component

--- 8.Let'sGetClassy/src/components/User.js
+++ 8.Let'sGetClassy/src/components/User.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 // const User = (props) => {
 const User = ({name}) => {
+    const [count] = useState(0);
+    const [count2] = useState(1);
     return (
     <div className = "user-card">
+        <h5>Count = {count}</h5>
+        <h5>Count2 = {count2}</h5>
         {/* <h2>Name: {props.name}</h2> */}
         {/* <h2>Name: Tanu</h2> */}
         <h2>Name: {name}</h2>
--- 8.Let'sGetClassy/src/components/UserClass.js
+++ 8.Let'sGetClassy/src/components/UserClass.js
@@ -1,24 +1,31 @@
 //Creating User component using Class Based Component
-
 import React from "react";
-
 class UserClass extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log(props);
+        // console.log(props);
+
+        // Creating State Variable using class component
+        this.state = {
+            count: 0,
+            count2: 1
+        }
     }
 
     render() {
-
         const {location} = this.props
+        const {count2} = this.state
+
         return (
          <div className = "user-card">
-             {/* <h2>Name: Tanu</h2> */}
-             <h2>Name: {this.props.name}</h2>
-              {/* <h3>Location: Kolkata</h3> */}
-              <h3>Location: {location}</h3>
-               <h4>Contact: @tanunest</h4>
+            <h5>Count = {this.state.count}</h5>
+            <h5>Count2 = {count2}</h5>
+            {/* <h2>Name: Tanu</h2> */}
+            <h2>Name: {this.props.name}</h2>
+            {/* <h3>Location: Kolkata</h3> */}
+            <h3>Location: {location}</h3>
+            <h4>Contact: @tanunest</h4>
          </div>
          )
     }
