96beba2b1caef1f0eea3ca319249a860dfbc63d7
Swati Tanu <swatitani01@gmail.com>
2023-12-04 22:13:13 +0530

    API call using class based component

--- 8.Let'sGetClassy/src/components/About.js
+++ 8.Let'sGetClassy/src/components/About.js
@@ -9,17 +9,43 @@ class About extends React.Component{
         super(props);
 
         console.log("1. Parent Constructor")
+
+        this.state = {
+            userInfo: {
+                name: "Dummy",
+                location: "Default",
+                avatar_url: "https://dummyphoto.com"
+            }
     }
+}
 
     // We use componentDidMount to make API call, why? Refer Notes.
-    componentDidMount() {
+    async componentDidMount() {
         console.log("6. Parent Component Did Mount")
 
         // API call
+        const data = await fetch("https://api.github.com/users/swati-tanu");
+        const json = await data.json();
+
+        this.setState({
+            userInfo: json
+        })
+
+        console.log(json);
+    }
+
+    componentDidUpdate() {
+        console.log("Component Did Update");
+    }
+
+    componentWillUnmount(){
+        console.log("Component Will Unmount");
     }
 
     render () {
        console.log("2. Parent Render")
+
+       const {location, avatar_url} = this.state.userInfo
        return (
         <div>
             <h1>About Class Component</h1>
@@ -29,6 +55,9 @@ class About extends React.Component{
             <h3>Using Class based Component:</h3>
             <UserClass name={"Tanu (class component)"} location={"Kolkata"}/>
             <UserClass name={"Akshay Saini (class component)"} location={"Dehradun"}/>
+            <h2>Name: {this.state.userInfo.name}</h2>
+            <h2>Location: {location}</h2>
+            <img src={avatar_url} />
         </div>
        )
     }
